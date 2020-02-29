3e03d841f657f9cd3026a824fa9998f6267357cf
mariusz498 <hajdylamariusz@gmail.com>
2020-02-29 16:45:52 +0100

    entities WIP

--- src/main/java/com/kodilla/backend/domain/Location.java
+++ src/main/java/com/kodilla/backend/domain/Location.java
@@ -1,4 +1,65 @@
 package com.kodilla.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "locations")
+@Table(name = "LOCATIONS")
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "location_id_seq")
+    @NotNull
+    @Column(name = "location_id", unique = true)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "lattitude")
+    private Double lattitude;
+
+    @NotNull
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @NotNull
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "origin",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "order_id")
+    private List<Order> ordersFrom;
+
+    @NotNull
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "destination",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "order_id")
+    private List<Order> ordersTo;
 }
