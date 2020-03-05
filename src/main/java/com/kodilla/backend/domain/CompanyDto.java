92ebad8b179dd54f0d31e5a74c933c47020ca21a
mariusz498 <hajdylamariusz@gmail.com>
2020-03-05 16:04:31 +0100

    small fixes

--- src/main/java/com/kodilla/backend/domain/CompanyDto.java
+++ src/main/java/com/kodilla/backend/domain/CompanyDto.java
@@ -1,5 +1,6 @@
 package com.kodilla.backend.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,8 +11,12 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class CompanyDto {
+    @JsonProperty("id")
     private Long id;
+    @JsonProperty("login")
     private String login;
+    @JsonProperty("passwordMD5")
     private String passwordMD5;
+    @JsonProperty("orders")
     private List<Order> orders;
 }
