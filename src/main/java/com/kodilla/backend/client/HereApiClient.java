e8af3e80194c7bc83b3de710f5051f0c754b088f
mariusz498 <hajdylamariusz@gmail.com>
2020-03-08 23:07:47 +0100

    HERE APi WIP

---
+++ src/main/java/com/kodilla/backend/client/HereApiClient.java
@@ -0,0 +1,11 @@
+package com.kodilla.backend.client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class HereApiClient {
+
+}
