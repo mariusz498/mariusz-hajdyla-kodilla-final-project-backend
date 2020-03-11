9a1ecdf60d9cc1791105be19ffcf2e55a0cd49ac
mariusz498 <hajdylamariusz@gmail.com>
2020-03-11 13:30:29 +0100

    order processing ready

--- src/main/java/com/kodilla/backend/service/OrderService.java
+++ src/main/java/com/kodilla/backend/service/OrderService.java
@@ -1,6 +1,27 @@
 package com.kodilla.backend.service;
 
+import com.kodilla.backend.domain.OrderDto;
+import com.kodilla.backend.domain.OrderRequestDto;
+import com.kodilla.backend.mapper.OrderMapper;
+import com.kodilla.backend.order.OrderProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 
-    //TODO createOrder and make an entity, return OrderDto to frontend
+    @Autowired
+    private DbService dbService;
+
+    @Autowired
+    private OrderProcessor orderProcessor;
+
+    @Autowired
+    private OrderMapper orderMapper;
+    //TODO createOrder and make an entity, return Order to frontend
+
+    public void processNewOrder(OrderRequestDto request) {
+        OrderDto createdOrderDto = orderProcessor.createOrder(request);
+        dbService.saveOrder(orderMapper.mapToOrder(createdOrderDto));
+    }
 }
